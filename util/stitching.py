d2779ab52abc34951aa2966da9387460af43e9bb
evanmoss <evan@hivemapper.com>
2024-01-09 02:02:57 -0800

    support restitching

--- util/stitching.py
+++ util/stitching.py
@@ -1,4 +1,9 @@
+import argparse
+import json
+import geopy.distance
 import numpy as np
+import os
+import uuid
 
 from datetime import datetime
 from dateutil import tz
@@ -6,6 +11,7 @@ from geographiclib.geodesic import Geodesic
 from pyproj import Transformer
 from scipy.spatial import KDTree
 from timezonefinder import TimezoneFinder
+from tqdm import tqdm
 from util import geo
 
 DEFAULT_STITCH_MAX_DISTANCE = 30 # 30 m
@@ -190,3 +196,118 @@ def stitch(
     print(f'Skipped {len(skipped)} paths.')
 
   return stitched + skipped
+
+def read_seqs(root, manifest, verbose):
+  if verbose:
+    print(f'Reading {root}')
+
+  contents = os.listdir(root)
+  for content in tqdm(contents):
+    p = os.path.join(root, content)
+    if os.path.isfile(p):
+      continue
+
+    seq_contents = os.listdir(p)
+    jpgs = set([f for f in seq_contents if f.endswith('.jpg')])
+    meta_path = os.path.join(p, 'meta.json')
+
+    with open(meta_path, 'r') as f:
+      meta = json.load(f)
+      for img in meta.keys():
+        if img in jpgs:
+          d = meta[img]
+          sid = f"{d['sequence']}/{d['idx']}.jpg"
+          manifest[sid] = d
+          manifest[sid]['loc'] = os.path.join(p, img)
+
+def traversal_gte(seq, min_seq_size):
+  total = 0
+  if len(seq) < 2:
+    return total
+
+  for i in range(1, len(seq)):
+    cur = (seq[i]['position']['lat'], seq[i]['position']['lon'])
+    prev = (seq[i - 1]['position']['lat'], seq[i - 1]['position']['lon'])
+    d = geopy.distance.distance(
+      cur,
+      prev,
+    ).meters
+    total += d
+
+    if total >= min_seq_size:
+      break
+
+  return total
+
+def restitch(
+  root,
+  outdir,
+  max_dist=DEFAULT_STITCH_MAX_DISTANCE,
+  max_lag=DEFAULT_STITCH_MAX_LAG,
+  max_angle=DEFAULT_STITCH_MAX_ANGLE,
+  min_seq_size=None,
+  verbose=True,
+):
+  manifest = {}
+  read_seqs(root, manifest, verbose)
+
+  frames = manifest.values()
+  stitched = stitch(frames, max_dist, max_lag, max_angle, verbose)
+
+  if verbose:
+    print(f'{len(stitched)} sequences')
+
+  if min_seq_size is not None:
+    if verbose:
+      print(f'Filtering out sequences < {min_seq_size} m')
+    stitched = [seq for seq in tqdm(stitched) if traversal_gte(seq, min_seq_size)]
+    if verbose:
+      print(f'{len(stitched)} sequences remaining')
+
+  if verbose:
+    print(f'Linking new sequences in {outdir}')
+
+  os.makedirs(outdir, exist_ok=True)
+
+  for i, seq in enumerate(tqdm(stitched)):
+    folder = f'{str(uuid.uuid4())}-{str(i)}'
+    local_dir = os.path.join(outdir, folder)
+    os.makedirs(local_dir, exist_ok=True)
+
+    local_meta_path = os.path.join(local_dir, 'meta.json')
+    img_paths = ["{}.jpg".format(i) for i in range(len(seq))]
+    meta = {
+      img_path:
+        { key: frame[key] for key in frame if key != 'url' }
+      for frame, img_path in zip(seq, img_paths)
+    }
+    with open(local_meta_path, 'w') as f:
+      json.dump(meta, f, indent=4)
+
+    for img_path, d in meta.items():
+      new_path = os.path.join(local_dir, img_path)
+      old_path = d['loc']
+      os.link(old_path, new_path)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-R', '--restitch', type=str)
+  parser.add_argument('-o', '--out', type=str)
+  parser.add_argument('-d', '--max_dist', type=float, default=DEFAULT_STITCH_MAX_DISTANCE)
+  parser.add_argument('-l', '--max_lag', type=float, default=DEFAULT_STITCH_MAX_ANGLE)
+  parser.add_argument('-z', '--max_angle', type=float, default=DEFAULT_STITCH_MAX_LAG)
+  parser.add_argument('-m', '--min_seq_size', type=float)
+  parser.add_argument('-v', '--verbose', action='store_true')
+  args = parser.parse_args()
+
+  if args.restitch:
+    assert(args.out)
+    restitch(
+      args.restitch,
+      args.out,
+      args.max_dist,
+      args.max_lag,
+      args.max_angle,
+      args.min_seq_size,
+      args.verbose,
+    )
