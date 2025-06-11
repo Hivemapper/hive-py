f80b43751d389372d8acb66b11554d1777d9783b
MattPeytonHivemapper <matthew@hivemapper.com>
2025-06-11 15:01:18 -0700

    use env var

--- tests/imagery/test_imagery_query.py
+++ tests/imagery/test_imagery_query.py
@@ -1,8 +1,12 @@
 import unittest
+import os
 
 class TestImageryQuery(unittest.TestCase):
     def test_query_frames(self):
         """Test for 'query_frames'"""
+        auth = os.getenv('HIVE_PY_UNIT_TEST_AUTH')
+        if not auth:
+            raise ValueError('HIVE_PY_UNIT_TEST_AUTH environment variable is not set')
         #todo mtp
 
 if __name__ == '__main__':
